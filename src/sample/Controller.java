9da0a3eaffd76f6b4526567ab140628ff8aa0796
Yulia <kisliuk-y@mail.ru>
2020-09-15 09:20:15 +0300

    ui

--- src/sample/Controller.java
+++ src/sample/Controller.java
@@ -2,10 +2,7 @@ package sample;
 
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
-import javafx.scene.chart.BarChart;
-import javafx.scene.chart.CategoryAxis;
-import javafx.scene.chart.NumberAxis;
-import javafx.scene.chart.XYChart;
+import javafx.scene.chart.*;
 import javafx.scene.control.Button;
 import sample.logic.LemerGenerator;
 import sample.logic.SequenceHandler;
@@ -17,7 +14,13 @@ public class Controller {
 
 
     @FXML
-    private BarChart<String, Number> barChart;
+    private AreaChart<Number, Number> barChart;
+
+    @FXML
+    private NumberAxis xAxis;
+
+    @FXML
+    private NumberAxis yAxis;
 
     @FXML
     private Button buildButton;
@@ -41,25 +44,8 @@ public class Controller {
     }
 
     private void configureBarChart() {
-        /*CategoryAxis xAxis = new CategoryAxis();
-        xAxis.setLabel("Интервал");
 
-        NumberAxis yAxis = new NumberAxis();
-        yAxis.setLabel("Значение плотности");
-
-        // Create a BarChart
-        //barChart = new BarChart<String, Number>(xAxis, yAxis);
-
-        // Series 1 - Data of 2014
-        XYChart.Series<String, Number> dataSeries1 = new XYChart.Series<String, Number>();
-        dataSeries1.setName("2014");
-
-        dataSeries1.getData().add(new XYChart.Data<String, Number>("0.5 - 0.6",0.973));
-        dataSeries1.getData().add(new XYChart.Data<String, Number>("0.55", 4.429));
-        dataSeries1.getData().add(new XYChart.Data<String, Number>("0.65", 2.792));
-        barChart.getData().add(dataSeries1);*/
-
-        LemerGenerator lg = new LemerGenerator(100, 3, 5, 1);
+        LemerGenerator lg = new LemerGenerator(1000, 3, 5 , 1);
         List<Float> sequence = lg.generateSequence();
         SequenceHandler sh = new SequenceHandler();
         sh.setSequenceSpecs(sequence);
@@ -67,15 +53,21 @@ public class Controller {
         System.out.println(Arrays.toString(sequence.toArray()));
         System.out.println(Arrays.toString(densityList.toArray()));
 
-        CategoryAxis xAxis = new CategoryAxis();
-        xAxis.setLabel("Интервал");
 
-        NumberAxis yAxis = new NumberAxis();
-        yAxis.setLabel("Значение плотности");
+        xAxis.setAutoRanging(false);
+        xAxis.setLowerBound(sh.getMinValue());
+        xAxis.setUpperBound(sh.getMaxValue());
+        xAxis.setTickUnit(sh.getStep());
+
+        yAxis.setAutoRanging(false);
+        yAxis.setLowerBound(0);
+        yAxis.setUpperBound(1);
+        yAxis.setTickUnit(0.05);
+
 
         //barChart = new BarChart<String, Number>(xAxis, yAxis);
 
-        XYChart.Series<String, Number> dataSeries1 = new XYChart.Series<String, Number>();
+        /*XYChart.Series<String, Number> dataSeries1 = new XYChart.Series<String, Number>();
         //dataSeries1.setName("2014");
 
         for(int i = 0; i < 20; i++) {
@@ -84,7 +76,16 @@ public class Controller {
             dataSeries1.getData().add(new XYChart.Data<String, Number>(String.valueOf(sh.getMinValue() + i * sh.getStep()),densityList.get(i)));
 
         }
-        barChart.getData().add(dataSeries1);
+        barChart.getData().add(dataSeries1);*/
+
+        XYChart.Series seriesDensity = new XYChart.Series();
+
+        for(int i = 0; i < 20; i ++) {
+            seriesDensity.getData().add(new XYChart.Data(sh.getMinValue() + i * sh.getStep(), densityList.get(i)));
+            seriesDensity.getData().add(new XYChart.Data(sh.getMinValue() + (i + 1) * sh.getStep(), densityList.get(i)));
+        }
+
+        barChart.getData().add(seriesDensity);
 
     }
 }
